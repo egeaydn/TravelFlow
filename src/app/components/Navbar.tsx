30b71538eeeae9fe1242f2aa9d0745f7851783f5
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2025-10-15 18:36:18 +0300

    Refactor layout and styling: remove unused font variables, simplify body styles, and add Navbar component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return(
+        <div className=" bg-amber-100 w-full h-16 flex items-center px-4">
+            <div className="align-center justify-center flex ">Logo</div>
+        </div>
+    )
+}
