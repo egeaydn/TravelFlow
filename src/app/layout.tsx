ae41018106a79d042494c596a32586d73a04c621
egeaydn <egeaydinn31@gmail.com>
2025-10-19 18:35:34 +0300

    navbar shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 export default function RootLayout({
   children,
@@ -9,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
       </body>
     </html>
